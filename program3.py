719e97420c2057281bf49d19c44f09e70569f691
harry2405 <77462332+harry2405@users.noreply.github.com>
2021-01-26 00:49:57 +0530

    Update program3.py

--- program3.py
+++ program3.py
@@ -1,69 +1,42 @@
+
 import pandas as pd
-from pandas import DataFrame
-df_tennis = pd.read_csv('myData.csv')
-print("\n Given Play Tennis Data Set:\n\n", df_tennis)
-
-
-def entropy(probs):
-    import math
-    return sum([-prob*math.log(prob, 2) for prob in probs])
-
-
-def entropy_of_list(a_list):
-    from collections import Counter
-    cnt = Counter(x for x in a_list)
-    num_instances = len(a_list)*1.0
-    probs = [x / num_instances for x in cnt.values()]
-    return entropy(probs)
-
-
-print("\n INPUT DATA SET FOR ENTROPY CALCULATION: \n",df_tennis['PlayTennis'])
-total_entropy = entropy_of_list(df_tennis['PlayTennis'])
-print("\n Total Entropy of PlayTennis Data Set:",total_entropy)
-
-
-def information_gain(df, split_attribute_name, target_attribute_name, trace=0):
-    df_split = df.groupby(split_attribute_name)
-    nobs = len(df.index) * 1.0
-    df_agg_ent = df_split.agg({target_attribute_name: [entropy_of_list, lambda x: len(x)/nobs]})[target_attribute_name]
-    df_agg_ent.columns = ['Entropy', 'PropObservations']
-    # Calculate Information Gain:
-    new_entropy = sum(df_agg_ent['Entropy'] * df_agg_ent['PropObservations'])
-    old_entropy = entropy_of_list(df[target_attribute_name])
-    return old_entropy - new_entropy
-
-
-def id3(df, target_attribute_name, attribute_names, default_class=None):
-    from collections import Counter
-    cnt = Counter(x for x in df[target_attribute_name])
-    if len(cnt) == 1:
-        return next(iter(cnt))
-    elif df.empty or (not attribute_names):
-        return default_class
-    else:
-        default_class = max(cnt.keys())
-
-    gainz = [information_gain(df, attr, target_attribute_name) for attr in attribute_names]
-    index_of_max = gainz.index(max(gainz))
-    best_attr = attribute_names[index_of_max]
-    tree = {best_attr: {}}
-    remaining_attribute_names = [i for i in attribute_names if i != best_attr]
-    for attr_val, data_subset in df.groupby(best_attr):
-        subtree = id3(data_subset,
-                      target_attribute_name,
-                      remaining_attribute_names,
-                      default_class)
-        tree[best_attr][attr_val] = subtree
-    return tree
-
-
-attribute_names = list(df_tennis.columns)
-print('List of Attributes:', attribute_names)
-attribute_names.remove('PlayTennis')
-print("Predicting Attributes:", attribute_names)
+from math import log
 from pprint import pprint
-tree = id3(df_tennis,'PlayTennis',attribute_names)
-print("\n\nThe Resultant Decision Tree is :\n")
-pprint(tree)
-attribute = next(iter(tree))
 
+df = pd.read_csv('myData.csv')
+data = df.values.tolist()
+attr_names = df.columns.values.tolist()
+
+def entropy(pos, neg):
+    if pos == 0 or neg == 0:
+        return 0
+    tot = pos + neg
+    return -pos / tot * log(pos / tot, 2) - neg / tot * log(neg / tot, 2)
+
+def gain(data, attr, pos, neg):
+    d, E, acu = {}, entropy(pos, neg), 0
+    for i in data:
+        if i[attr] not in d:
+            d[i[attr]] = {}
+        d[i[attr]][i[-1]] = 1 + d[i[attr]].get(i[-1], 0)
+    for i in d:
+        tot = d[i].get('Yes', 0) + d[i].get('No', 0)
+        acu += tot / (pos + neg) * entropy(d[i].get('Yes', 0), d[i].get('No', 0))
+    return E - acu
+
+def build(data, attr_names):
+    pos, sz = len([x for x in data if x[-1] == 'Yes']), len(data[0]) - 1
+    neg = len(data) - pos
+    if neg == 0 or pos == 0:
+        return 'Yes' if neg == 0 else 'No'
+
+    root = max([[gain(data, i, pos, neg), i] for i in range(sz)])[1]     
+    fin, res = {}, {}
+    uniq_attr = set([x[root] for x in data])
+    for i in uniq_attr:
+        res[i] = build([x[:root] + x[root + 1:] for x in data if x[root] == i], attr_names[:root] + attr_names[root+1:])
+    fin[attr_names[root]] = res
+    return fin
+
+tree = build(data, attr_names)
+pprint(tree)
